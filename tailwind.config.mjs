1fee5514b42d1d5e957479437a666c77365abbaf
Abdumuhammadjon <nestnext551@gmail.com>
2025-05-03 00:14:15 +0500

    rewtr4wt

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+module.exports = {
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        inter: ['var(--font-inter)', 'sans-serif'],
       },
     },
   },
-  plugins: [],
 };
+
